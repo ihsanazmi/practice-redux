375f42a87fb4590f4c158ee39da7d52723486117
ihsanazmi <ihsanazmiii@gmail.com>
2019-09-28 12:53:59 +0700

    App without redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+    
+    state = {count : 0}
+    
+    onClickTambah = ()=>{
+        this.setState({
+            count : this.state.count + 1
+        })
+    }
+
+    onClickKurang = ()=>{
+        this.setState({
+            count: this.state.count - 1
+        })
+    }
+
     render() {
         return (
             <div>
-               <h1>Test</h1> 
+                <center>
+                    <h1>{this.state.count}</h1> 
+                    <button className="btn btn-outline-danger" onClick={this.onClickKurang}>Kurang</button><span> </span>
+                    <button className="btn btn-outline-success" onClick={this.onClickTambah}>Tambah</button>
+                </center>
             </div>
         )
     }
